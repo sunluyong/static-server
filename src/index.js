5565788dc317f29372f6e67e6fd55ec92323d0ea
Samaritan89 <237732937@qq.com>
2020-05-16 19:46:19 +0800

    feat: 支持读取文件夹

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime-types');
+const handlebars = require('handlebars');
+const compare = require('natural-compare');
 
 const defaultConf = require('./config');
+
+const htmlTpl = fs.readFileSync(path.join(__dirname, './tpl/directory.hbs'));
+const template = handlebars.compile(htmlTpl.toString());
 
 class StaticServer {
   constructor(options = {}) {
@@ -32,10 +37,48 @@ class StaticServer {
           res.end('文件不存在！');
 
         } else {
-          res.writeHead(200, {
-            'content-type': mime.contentType(path.extname(url)),
-          });
-          fs.createReadStream(filePath).pipe(res);
+          const stats = fs.statSync(filePath);
+          const list = [];
+          if (stats.isDirectory()) {
+            // 如果是文件夹则遍历文件夹，生成改文件夹内的文件树
+            const dir = fs.opendirSync(filePath);
+            let dirent = dir.readSync();
+            while (dirent) {
+              list.push({
+                name: dirent.name,
+                path: path.join(url, dirent.name),
+                type: dirent.isDirectory() ? 'folder' : 'file',
+              });
+              dirent = dir.readSync();
+            }
+            dir.close();
+
+            res.writeHead(200, {
+              'content-type': 'text/html',
+            });
+
+            // 对文件顺序重排，文件夹在文件前面，相同类型按字母排序，不区分大小写
+            list.sort((x, y) => {
+              if (x.type > y.type) {
+                // 'folder' > 'file'， 返回 -1，folder 在 file 之前
+                return -1;
+              } else if (x.type == y.type) {
+                return compare(x.name.toLowerCase(), y.name.toLowerCase());
+              } else {
+                return 1;
+              }
+            })
+
+            // 使用 handlebars 模板引擎，生成目录页面 html
+            const html = template({ list });
+            res.end(html);
+
+          } else {
+            res.writeHead(200, {
+              'content-type': mime.contentType(path.extname(url)),
+            });
+            fs.createReadStream(filePath).pipe(res);
+          }
         }
       });
     }).listen(port, () => {
